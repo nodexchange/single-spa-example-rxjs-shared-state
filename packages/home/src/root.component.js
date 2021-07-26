ae920bc8ef2612ca191c7d0d5dd441e97c39438d
Marcin Wojtala <marcin.wotjala@bottomline.com>
2021-07-26 15:01:52 +0100

    play with shared redps

--- packages/home/src/root.component.js
+++ packages/home/src/root.component.js
@@ -2,15 +2,17 @@ import React from "react";
 import { I18nContext, ThemeProvider, nextTheme, i18n } from "@example/design-system";
 import { Button, CustomButton } from '@example/design-system';
 import Grid from './components/grid/Grid';
+
 const Home = (props) => {
   return (
-    <ThemeProvider theme={{ modal: { width: 500 } }}>
+    <ThemeProvider baseTheme={nextTheme} theme={{ modal: { width: 500 } }}>
       <I18nContext.Provider value={i18n.english}>
         <CustomButton text={'hello home'} />
         <section style={{ textAlign: "center", padding: "1rem" }}>
           <Grid />
+          <p>Hello</p>
           {props.name} is mounted today?!
-          <Button text={'Glu button'} />
+          <Button text={'Glu'} />
           <p>hello now? fast?</p>
           <p>customProp: {props.bottomlineProp}</p>
         </section>
